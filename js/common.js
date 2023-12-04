ad66c271a0001f7b6556b4f721be6559f1cc9e65
Suhyun Kim <kim00431@algonquinlive.com>
2023-12-03 20:40:05 -0500

    updates

---
+++ js/common.js
@@ -0,0 +1,17 @@
+// let dragged;
+// const tree = document.querySelector('.tree_container')
+// /* 드래그 가능한 대상에서 발생하는 이벤트 */
+
+// tree.addEventListener("dragstart", (event) => {
+//     // 드래그한 요소에 대한 참조 저장
+//     dragged = event.target;
+//     // 반투명하게 만들기
+//     dragged.style.transform = 'scale(2)';
+//   });
+
+//   tree.addEventListener("dragenter", (event) => {
+//     // 드래그 가능한 요소가 대상 위로 오면 강조
+//     event.target.style.opacity = 0.5;
+//   });
+
+
